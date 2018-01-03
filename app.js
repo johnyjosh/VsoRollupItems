61b95b0f7137d934479a9cfd1016584e3f23f58c
Josh George <joshge@microsoft.com>
2018-01-03 00:26:05 -0800

    Move personal access token to a separate file

--- app.js
+++ app.js
@@ -6,10 +6,16 @@
 "use strict";
 const request = require("request-promise");
 const q = require('q');
+const path = require('path');
 const _ = require('lodash');
+const fs = require('fs');
 const cmdargs = require('command-line-args'); // https://www.npmjs.com/package/command-line-args
 
-const encodedPat = encodePat('{PUT IN YOUR OWN PRIVATE ACCESS TOKEN}');
+// Access the personal access token from a local file you need to create
+// Info on how to create PAT token: https://docs.microsoft.com/en-us/vsts/accounts/use-personal-access-tokens-to-authenticate
+const patFile = path.dirname(require.main.filename) + '\\personal_access_token.json';
+const pat = JSON.parse(fs.readFileSync(patFile));
+const encodedPat = encodePat(pat.token);
 
 const queryId = 'ebdee26e-0fa5-4330-8df4-db07b131ab38';
 const vstsConstants = {
@@ -21,23 +27,7 @@ const vstsConstants = {
   maxUpdates: 100 // Just a safety net in case there is a logical bug in the code that results in too many work items being updated
 };
 
-const options = [
-    { name: 'forcecap', type: Number },
-    { name: 'showupdatesonly', type: Boolean },
-    { name: 'safe', alias: 'n', type: Boolean}
-];
-const args = cmdargs(options);
-
-if (args.forcecap) {
-    console.log(`Capping updates to max of ${args.forcecap}.`);
-}
-if (args.safe) {
-    console.log(`Safe mode enabled, updates will be computed, but not persisted to VSO.`);
-}
-
-if (args.showupdatesonly) {
-    console.log(`Entries that are computed and then skipped will not be shown in the console output.`);
-}
+const args = processCommandline();
 
 // 1. Execute the query to retrieve the hierarchy of work items in the given area path.
 // Note that the query is from WorkItemLinks rather than from WorkItems.
@@ -77,6 +67,12 @@ vstsApi('POST', `${vstsConstants.baseVstsUri}/${vstsConstants.project}/_apis/wit
     // 3. Query for additional fields for each of the work items
     return getWorkItemFields(_.map(queryResults.workItemRelations, x => x.target.id))
     .then(workItemsWithFields => {
+        // We create a wrapper object vsoitems that has two objects:
+        // workItemsHierarchy and workItemsWithFields
+        // Both are indexed by work item ID.
+        // 1. workItemsHierarchy[id] is an array of direct child work item IDs (not recursive).
+        // 2. workItemsWithFields[id].fields has the cost info from VSO.
+        //    workItemsWithFields[id].state is where the computed rollup and misc. state flags are maintained.
         return {
             workItemsHierarchy: workItemsHierarchy,
             workItemsWithFields: workItemsWithFields
@@ -94,69 +90,80 @@ vstsApi('POST', `${vstsConstants.baseVstsUri}/${vstsConstants.project}/_apis/wit
 .then((vsoItems) => {
     // Print out the update plan for reference
     // TODO: Make this optional based on parameter.
-    var shortenedFieldNames = _.map(vstsConstants.fieldsToRollup, fieldName => _.replace(fieldName, "Microsoft.VSTS.Scheduling.", ""));
-    console.log("id\tTitle\tworkItemType\tState\tisSelectedForUpdate\t" + _.join(shortenedFieldNames, "(Old)\t") + "(Old)\t" + _.join(shortenedFieldNames, "(New)\t") + "(New)");
-    _.each(vsoItems.workItemsWithFields, (workItemFields, id) => {
-        var isSelected = selectionCriteria(vsoItems, id);
-        var fieldValuesOld = "", fieldvaluesNew = "";
-        _.each(vstsConstants.fieldsToRollup, fieldName => {
-            fieldValuesOld += workItemFields.fields[fieldName] + "\t";
-            fieldvaluesNew += workItemFields.state.rollupInfo[fieldName] + "\t";
-        });
-        if (!args.showupdatesonly || isSelected) {
-            console.log(`${id}\t"${workItemFields.fields['System.Title']}"\t${workItemFields.fields['System.WorkItemType']}\t${workItemFields.fields['System.State']}\t${isSelected}\t` + fieldValuesOld + fieldvaluesNew);
-        }
-    });
+    printUpdatePlan(vsoItems);
     return vsoItems; // Just pass it down the chain
 })
 .then((vsoItems) => {
     if (args.safe) {
         console.log("Skipping update because safe mode is enabled.");
+        return 0;
     } else {
         console.log("Writing updated costs into VSO.");
         return updateCosts(vsoItems);
     }
 })
+.then((updatedItemsCount) => {
+    console.log(`Count of vso items updated: ${updatedItemsCount}`);
+})
 .catch(error => {
     console.log("Encountered an error: " + error);
 })
 .finally(() => {
-    console.log("Program is done.");
+    console.log(`Done`);
 });
+
+function printUpdatePlan(vsoItems) {
+    const shortenedFieldNames = _.map(vstsConstants.fieldsToRollup, fieldName => _.replace(fieldName, "Microsoft.VSTS.Scheduling.", ""));
+    console.log("id\t" + _.join(shortenedFieldNames, "(Old)\t") + "(Old)\t" + _.join(shortenedFieldNames, "(New)\t") + "(New)\t" + "isUpdateRequired\tworkItemType\tState\tTitle");
+    _.each(vsoItems.workItemsWithFields, (workItemFields, id) => {
+        var fieldValuesOld = "", fieldvaluesNew = "";
+        _.each(vstsConstants.fieldsToRollup, fieldName => {
+            fieldValuesOld += workItemFields.fields[fieldName] + "\t";
+            fieldvaluesNew += workItemFields.state.rollupInfo[fieldName] + "\t";
+        });
+        if (args.verbose || workItemFields.state.isUpdateRequired) {
+            console.log(`${id}\t` + fieldValuesOld + fieldvaluesNew + `${workItemFields.state.isUpdateRequired}\t${workItemFields.fields['System.WorkItemType']}\t${workItemFields.fields['System.State']}\t"${workItemFields.fields['System.Title']}"`);
+        }
+    });
+}
 
 // If a valid rollup value could be computed (for example during rollup we don't even visit orphaned items),
 // and if it is not the same as what is already in VSO, only then do we want to update.
-function selectionCriteria(vsoItems, id) {
-    var workItemFields = vsoItems.workItemsWithFields[id];
+function evaluateUpdateRequired(workItemFields) {
     var netSum = 0;
-    var isUpdatedValue = _.some(vstsConstants.fieldsToRollup, fieldName => {
+    var isSomeValueUpdated = _.some(vstsConstants.fieldsToRollup, fieldName => {
         var rollupValue = workItemFields.state.rollupInfo[fieldName];
         var currentValue = workItemFields.fields[fieldName];
         netSum += rollupValue + currentValue;
         return (rollupValue != undefined && currentValue != rollupValue);
     });
 
-    // This catches the case where old values are undefined and new values are zeroes, but in effect there is no net update
-    return netSum > 0 && isUpdatedValue;
+    // netSum is computed across all the fields and the intent is to leave untouched any newly created epics, features, requirements, etc
+    // that haven't been costed at all and all the cost fields are blank currently.
+    return netSum > 0 && isSomeValueUpdated;
 }
 
 // Write the computed costs back to VSO, use batched updates for reducing round trips
 function updateCosts(vsoItems) {
-    var ids = _.map(vsoItems.workItemsWithFields, (v,id) => id);
+    const ids = _.map(vsoItems.workItemsWithFields, (v,id) => id);
 
-    var filteredIds = _.filter(ids, (id) => selectionCriteria(vsoItems, id));
-    var cappedIds = _.take(filteredIds, args.forcecap || vstsConstants.maxUpdates);
+    const filteredIds = _.filter(ids, (id) => vsoItems.workItemsWithFields[id].state.isUpdateRequired);
+    const cappedIds = _.take(filteredIds, args.forcecap || vstsConstants.maxUpdates);
     if (filteredIds.length != cappedIds.length) {
         console.error(`Warning: ${filteredIds.length} updates computed, capping to ${cappedIds.length}`);
     }
 
-    var chunkedIds = _.chunk(cappedIds, vstsConstants.batchSize);
+    const chunkedIds = _.chunk(cappedIds, vstsConstants.batchSize);
 
     var deferred = q.defer();
+    if (chunkedIds.length == 0) {
+        return deferred.resolve(0);
+    }
+
     var resultPromises = [];
 
     _.each(chunkedIds, idsToUpdate => {
-        var patchRequests = _.map(idsToUpdate, id => {
+        const patchRequests = _.map(idsToUpdate, id => {
             var patchOp = {
                 "method": "PATCH",
                 "uri" : `/_apis/wit/workitems/${id}?api-version=1.0`,
@@ -179,37 +186,33 @@ function updateCosts(vsoItems) {
   
         var deferred = q.defer();
         resultPromises.push(deferred.promise);
+        
         vstsApi('POST', `${vstsConstants.baseVstsUri}/_apis/wit/$batch`, patchRequests)
-        .then(x => deferred.resolve())
+        .then(x => deferred.resolve(idsToUpdate.length))
         .catch(error => deferred.reject(error));
     });
 
     q.all(resultPromises)
-    .then(x => deferred.resolve())
+    .then(resultArray => {
+        deferred.resolve(_.reduce(resultArray, (sum, x) => sum + x, 0))
+    })
     .catch(error => deferred.reject(error));
 
     return deferred.promise;
 }
 
-// Expects an object with two fields:
-// workItemsHierarchy and workItemsWithFields
-// Both are indexed by work item ID.
-// workItemsHierarchy[id] is an array of direct chid work item IDs (not recursive).
-// workItemsWithFields[id].fields has the cost info from VSO.
-// workItemsWithFields[id].state will be used to maintain state on whether a given
-// item has been rolled up yet.
 function rollupCosts(vsoItems) {
     _.each(vsoItems.workItemsHierarchy, (childIds, itemId) => rollupCostsForItem(vsoItems, itemId));
 }
 
 function rollupCostsForItem(vsoItems, itemId) {
-    var workItemFields = vsoItems.workItemsWithFields[itemId];
-    if (workItemFields.state['isProcessed']) {
+    const workItemFields = vsoItems.workItemsWithFields[itemId];
+    if (workItemFields.state.isProcessed) {
         // If roll up is already done for this item, then this is a no-op call.
         return;
     }
 
-    var hierarchyInfo = vsoItems.workItemsHierarchy[itemId];
+    const hierarchyInfo = vsoItems.workItemsHierarchy[itemId];
     if (hierarchyInfo == undefined) {
         // For leaf nodes, we just copy the item's costs into the state.rollupInfo structure.
         _.each(vstsConstants.fieldsToRollup, fieldName => workItemFields.state.rollupInfo[fieldName] = workItemFields.fields[fieldName] || 0);
@@ -226,7 +229,8 @@ function rollupCostsForItem(vsoItems, itemId) {
 
     }
 
-    workItemFields.state['isProcessed'] = true;
+    workItemFields.state.isProcessed = true;
+    workItemFields.state.isUpdateRequired = evaluateUpdateRequired(workItemFields);
 }
 
 // Expects an array of work item IDs.
@@ -234,7 +238,7 @@ function rollupCostsForItem(vsoItems, itemId) {
 // with all the roll up fields as well as work item type.
 function getWorkItemFields(ids) {
     var deferred = q.defer();
-    var chunkedIds = _.chunk(ids, vstsConstants.maxIdsInSingleCall);
+    const chunkedIds = _.chunk(ids, vstsConstants.maxIdsInSingleCall);
     var isError = false;
 
     // We will do batched queries using a VSO REST API that can take up to 200
@@ -248,8 +252,8 @@ function getWorkItemFields(ids) {
     _.each(chunkedIds, idsCapped => {
         var deferred = q.defer();
         resultPromises.push(deferred.promise);
-        var idsCappedJoined = _.join(idsCapped);
-        var fields = 'System.Id,System.Title,System.WorkItemType,System.State,' + _.join(vstsConstants.fieldsToRollup);
+        const idsCappedJoined = _.join(idsCapped);
+        const fields = 'System.Id,System.Title,System.WorkItemType,System.State,' + _.join(vstsConstants.fieldsToRollup);
         vstsApi('GET', `${vstsConstants.baseVstsUri}/_apis/wit/workitems?ids=${idsCappedJoined}&fields=${fields}`)
         .then(response => {
             // The response is {count, value} where value is the array of VSO items.
@@ -260,7 +264,8 @@ function getWorkItemFields(ids) {
                     fields: workItemFields.fields,
                     state: {  // Will be used later
                         rollupInfo: {},
-                        isProcessed: false
+                        isProcessed: false,
+                        isUpdateRequired: false
                     }
                 }
             });
@@ -282,7 +287,7 @@ function getWorkItemFields(ids) {
 // uri: The HTTP endpoint
 // body: Optional body parameter
 function vstsApi(method, uri, body) {
-    var options = {
+    const options = {
         method: method,
         headers: { 'cache-control': 'no-cache', 'authorization': `Basic ${encodedPat}` },
         uri: uri,
@@ -299,8 +304,48 @@ function vstsApi(method, uri, body) {
 }
 
 function encodePat(pat) {
-    var b = new Buffer(':' + pat);
-    var s = b.toString('base64');
+    const b = new Buffer(':' + pat);
+    return b.toString('base64');
+}
 
-    return s;
+function showHelp() {
+    console.log(`--forcecap N (or -f N)`);
+    console.log(`   Forces that no more than N entries will be updated in a single run of the program.`);
+    console.log(`   Best to set some low limits like 1 or 2 items until you are sure the program is doing what you expect.`);
+    console.log('');
+    console.log(`--verbose (or -v)`);
+    console.log(`   By default the program will spit out a tab separated table of any updates it will apply after the rollups are calculated.`);
+    console.log(`   In verbose mode, the program will also show the set of rows that were computed but skipped for update because`);
+    console.log(`   the computed values were the same as the old values.`);
+    console.log('');
+    console.log(`--safe (or -n)`);
+    console.log(`   Do the rollup calculations, but don't actually commit to VSO.`);
+}
+
+function processCommandline() {
+    const options = [
+        { name: 'forcecap', alias: 'f', type: Number },
+        { name: 'verbose', alias: 'v', type: Boolean },
+        { name: 'safe', alias: 'n', type: Boolean},
+        { name: 'help', alias: 'h', type: Boolean}
+    ];
+    const args = cmdargs(options);
+    
+    if (args.help) {
+        showHelp();
+        return;
+    }
+    
+    if (args.forcecap) {
+        console.log(`Capping updates to max of ${args.forcecap}.`);
+    }
+    if (args.safe) {
+        console.log(`Safe mode enabled, updates will be computed, but not persisted to VSO.`);
+    }
+    
+    if (args.verbose) {
+        console.log(`Override the default behavior that does not show the entries that are skipped from being updated.`);
+    }
+
+    return args;
 }
